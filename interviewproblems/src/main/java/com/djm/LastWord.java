4e370f96b35f01ff8b7087cf202b6a719fa461ca
derekmccool <mccool.derek@yahoo.com>
2021-01-07 13:00:15 -0600

    Create LastWord.java

---
+++ interviewproblems/src/main/java/com/djm/LastWord.java
@@ -0,0 +1,17 @@
+package com.cognixia.jump.corejava.strings;
+
+public class LastWord {
+    
+    public static void main(String[] args) {
+        String sentence1 = "The quick brown fox jumps over the lazy dog";
+        String sentence2 = "Waffles are the best breakfast";
+
+        System.out.println("The length of the last word of: " + sentence1.toUpperCase() + " is: " + lastWordLength(sentence1));
+        System.out.println("The length of the last word of: " + sentence2.toUpperCase() + " is: " + lastWordLength(sentence2));
+
+    }
+    public static int lastWordLength(String sentence){
+        String[] words = sentence.split(" ");
+        return words[words.length -1].length();
+    }
+}
